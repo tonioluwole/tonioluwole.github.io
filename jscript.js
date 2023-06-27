81d2e026af47940b47dfede15baf50849d5be789
Toni Oluwole <59587344+tonioluwole@users.noreply.github.com>
2023-06-27 07:33:33 -0400

    organized stuff

--- jscript.js
+++ jscript.js
@@ -2,27 +2,3 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     document.location = "https://tonioluwole.github.io/mobiletoni.github.io/";
 }
-
-const text = "Hello, I'm a JavaScript Developer."; // The text to display in the typing effect
-const typingSpeed = 100; // The speed of typing (in milliseconds)
-const delayAfterTyping = 1000; // The delay after the text is fully typed (in milliseconds)
-
-const typingElement = document.querySelector(".typing-effect");
-
-let charIndex = 0;
-
-function typeText() {
-    if (charIndex < text.length) {
-        typingElement.textContent += text.charAt(charIndex);
-        charIndex++;
-        setTimeout(typeText, typingSpeed);
-    } else {
-        setTimeout(() => {
-            typingElement.textContent = "";
-            charIndex = 0;
-            typeText();
-        }, delayAfterTyping);
-    }
-}
-
-typeText();
