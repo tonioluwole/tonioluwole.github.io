13d74db40f69781ad1dc40e54eea5b3057338419
Toni Oluwole <Toni.Oluwole@gmail.com>
2024-01-29 02:45:54 -0500

    fixed hero typing, table format and updated resumes

--- jscript.js
+++ jscript.js
@@ -3,84 +3,53 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     document.location = "https://tonioluwole.github.io/mobiletoni.github.io/";
 }
 
-// List of sentences
-var _CONTENT = [
-    "Designer",
-    "Programmer",
-    "Writer",
-    "Forever Learner",
-    "Problem Solver"
-];
+const carouselText = [
 
-// Current sentence being processed
-var _PART = 0;
-
-// Character number of the current sentence being processed 
-var _PART_INDEX = 0;
-
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL;
-
-// Element that holds the text
-var _ELEMENT = document.querySelector("#poppinsmain");
-
-// Starts typing effect
-function Type() { 
-    var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX++;
-
-    // If full sentence has been displayed then start to delete the sentence after some time
-    if(text === _CONTENT[_PART]) {
-        clearInterval(_INTERVAL_VAL);
-        setTimeout(function() {
-            _INTERVAL_VAL = setInterval(Delete, 40);
-        }, 900);
+    {text: "Tech Enthusiast"},
+    {text: "Developer"},
+    {text: "Writer"},
+    {text: "Quick Learner"},
+    {text: "Problem Solver"}
+  ]
+  
+  $( document ).ready(async function() {
+    carousel(carouselText, "#feature-text")
+  });
+  
+  async function typeSentence(sentence, eleRef, delay = 50) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      $(eleRef).append(letters[i]);
+      i++
     }
-}
-
-// Starts deleting effect
-function Delete() {
-    var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX--;
-
-    // If sentence has been deleted then start to display the next sentence
-    if(text === '') {
-        clearInterval(_INTERVAL_VAL);
-
-        // If last sentence then display the first one, else move to the next
-        if(_PART == (_CONTENT.length - 1))
-            _PART = 0;
-        else
-            _PART++;
-        _PART_INDEX = 0;
-
-        // Start to display the next sentence after some time
-        setTimeout(function() {
-            _INTERVAL_VAL = setInterval(Type, 100);
-        }, 200);
+    return;
+  }
+  
+  async function deleteSentence(eleRef) {
+    const sentence = $(eleRef).html();
+    const letters = sentence.split("");
+    let i = 0;
+    while(letters.length > 0) {
+      await waitForMs(50);
+      letters.pop();
+      $(eleRef).html(letters.join(""));
     }
-}
-
-// Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 100);
-
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel( {
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
-});
+  }
+  
+  async function carousel(carouselList, eleRef) {
+      var i = 0;
+      while(true) {
+        await typeSentence(carouselList[i].text, eleRef);
+        await waitForMs(1000);
+        await deleteSentence(eleRef);
+        await waitForMs(200);
+        i++
+        if(i >= carouselList.length) {i = 0;}
+      }
+  }
+  
+  function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
